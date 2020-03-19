99773971aca22f28b5c7f4db4d1a038ff34eb30b
Gopi K <k.gopi1@gmail.com>
2020-03-19 18:19:15 +0000

    Update main.tf

--- az/resource_group/main.tf
+++ az/resource_group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "resource_group" {
-  for_each = var.resource_group_name
+  for_each = toset(var.resource_group_name)
   name     = each.key
   location = var.resource_group_location
 }
