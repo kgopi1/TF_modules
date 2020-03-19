4fe8b3b240bd9e898f3d1a24c14722dd874fca00
Gopi K <k.gopi1@gmail.com>
2020-03-19 21:43:24 +0000

    Update outputs.tf

--- az/resource_group/outputs.tf
+++ az/resource_group/outputs.tf
@@ -1,3 +1,4 @@
-# output "resource_group_name" {
-#   value = [azurerm_resource_group.resource_group.*.id]
-# }
+output "resource_group_name" {
+  #value = [azurerm_resource_group.resource_group.*.id]
+  value = [ for v in azurerm_resource_group.resource_group : v.id ]
+}
