024af1afc1c41fa558352cec3314aaa2d74d97f1
Gopi K <k.gopi1@gmail.com>
2020-03-19 17:24:21 +0000

    azure resource group module

---
+++ resource_group/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  description = "Resource Group name"
+  type    = string 
+}
+
+variable "resource_group_location" {
+  description = "Resource Group location"
+  type    = string 
+}
+
